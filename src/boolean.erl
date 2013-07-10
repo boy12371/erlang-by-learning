13530c1b6e23e073694d08ebdb54f5be84cd9ba7
boy12371 <boy12371@hotmail.com>
2013-07-10 23:47:05 +0800

    update boolean.erl

--- src/boolean.erl
+++ src/boolean.erl
@@ -1,3 +1,16 @@
+% <Erlang Programming>Exercise 2-3: Simple Pattern Matching
+%
+% Write a module boolean.erl that takes logical expressions and Boolean values (repre-
+% sented as the atoms true and false) and returns their Boolean result. The functions
+% you write should include b_not/1, b_and/2, b_or/2, and b_nand/2. You should not use
+% the logical constructs and, or, and not, but instead use pattern matching to achieve
+% your goal.
+%
+% Richard Kuang
+% kuangyel2000@gmail.com
+% July 10, 2013
+
+
 -module(boolean).
 -export([b_not/1, b_and/2, b_or/2, b_nand/2]).
 
