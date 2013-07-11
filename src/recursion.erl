094a26357b6245082def8d3394ffe14a76f37910
boy12371 <boy12371@hotmail.com>
2013-07-11 21:11:01 +0800

    update recursion.erl, add tail_recursion.erl

--- src/recursion.erl
+++ src/recursion.erl
@@ -1,16 +1,27 @@
-% learning recursion
+%% @author Richard
+%% learning recursion
+
 
 -module(recursion).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
 -export([bump/1, average/1, even/1, member/2]).
 
-% taking a list of integers and adding 1 to every element in the list
-
+%% ====================================================================
+%% bump functions
+%% taking a list of integers and adding 1 to every element in the list
+%% ====================================================================
 bump([]) -> [];
 bump([Head | Tail]) ->
     [Head + 1 | bump(Tail)].
 
-% compute the average of a list of numbers
 
+%% ====================================================================
+%% average functions
+%% compute the average of a list of numbers
+%% ====================================================================
 average([]) -> 0;
 average(List) ->
     sum(List) / len(List).
@@ -23,19 +34,27 @@ len([]) -> 0;
 len([_ | Tail]) ->
     1 + len(Tail).
 
-% traverse a list, filtering out the elements that are not even
 
+%% ====================================================================
+%% even functions
+%% traverse a list, filtering out the elements that are not even
+%% ====================================================================
 even([]) -> [];
 even([Head | Tail]) when is_integer(Head), Head rem 2 == 0 ->
     [Head | even(Tail)];
 even([_ | Tail]) ->
     even(Tail).
 
-% if the list contains at least one element, and we check whether the first element is the one we are looking for
 
+%% ====================================================================
+%% member functions
+%% if the list contains at least one element, and we check whether the
+%% first element is the one we are looking for
+%% ====================================================================
 member(_, []) ->
     false;
 member(H, [H | _]) ->
     true;
 member(H, [_ | T]) ->
     member(H, T).
+
---
+++ src/tail_recursion.erl
@@ -0,0 +1,86 @@
+%% @author Richard
+%% learning tail recursion
+
+
+-module(tail_recursion).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([bump/1, average/1, even/1, member/2]).
+
+
+
+%% ====================================================================
+%% bump functions
+%% taking a list of integers and adding 1 to every element in the list
+%% ====================================================================
+bump(List) ->
+    bump(List, []).
+
+bump([], List) ->
+    reverse(List);
+bump([H | T], List) ->
+    bump(T, [H + 1 | List]).
+
+
+%% ====================================================================
+%% average functions
+%% compute the average of a list of numbers
+%% ====================================================================
+average(List) ->
+    average(List, 0, 0).
+
+average([], _, 0) -> 0;
+average([], Sum, Len) ->
+    Sum / Len;
+average([H | T], Sum, Len) ->
+    average(T, Sum + H, Len + 1).
+
+
+%% ====================================================================
+%% even functions
+%% traverse a list, filtering out the elements that are not even
+%% ====================================================================
+even(List) ->
+    even(List, []).
+
+even([], List) ->
+    reverse(List);
+even([H | T], List) when is_integer(H), H rem 2 == 0 ->
+    even(T, [H | List]);
+even([_ | T], List) ->
+    even(T, List).
+
+
+%% ====================================================================
+%% member functions
+%% if the list contains at least one element, and we check whether the
+%% first element is the one we are looking for
+%% ====================================================================
+member(X, List) ->
+    member(X, List, false).
+
+member(_, [], Bool) ->
+    Bool;
+member(H, [H | _], Bool) ->
+    not(Bool);
+member(H, [_ | T], Bool) ->
+    member(H, T, Bool).
+
+
+%% ====================================================================
+%% reverse functions
+%% an accumulator-based reverse function
+%% ====================================================================
+reverse(List) ->
+    reverse(List, []).
+
+reverse([H | T], List) ->
+    reverse(T, [H | List]);
+reverse([], List) ->
+    List.
+
+
+
+
