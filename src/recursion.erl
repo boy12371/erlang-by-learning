11ce817303b0964f0338cdaac9cdea3610a13df1
boy12371 <boy12371@hotmail.com>
2013-07-11 16:08:40 +0800

    delete bump.erl, average.erl, even.erl and add recursion.erl

---
+++ src/recursion.erl
@@ -0,0 +1,41 @@
+% learning recursion
+
+-module(recursion).
+-export([bump/1, average/1, even/1, member/2]).
+
+% taking a list of integers and adding 1 to every element in the list
+
+bump([]) -> [];
+bump([Head | Tail]) ->
+    [Head + 1 | bump(Tail)].
+
+% compute the average of a list of numbers
+
+average([]) -> 0;
+average(List) ->
+    sum(List) / len(List).
+
+sum([]) -> 0;
+sum([Head | Tail]) ->
+    Head + sum(Tail).
+
+len([]) -> 0;
+len([_ | Tail]) ->
+    1 + len(Tail).
+
+% traverse a list, filtering out the elements that are not even
+
+even([]) -> [];
+even([Head | Tail]) when is_integer(Head), Head rem 2 == 0 ->
+    [Head | even(Tail)];
+even([_ | Tail]) ->
+    even(Tail).
+
+% if the list contains at least one element, and we check whether the first element is the one we are looking for
+
+member(_, []) ->
+    false;
+member(H, [H | _]) ->
+    true;
+member(H, [_ | T]) ->
+    member(H, T).
